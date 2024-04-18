9caac05a06781b232428fbcd84fd0155e16a1f8c
Dinuka <dinukanh@gmail.com>
2024-04-18 22:41:11 +1000

    Sequence animation and sound presenting

--- game.js
+++ game.js
@@ -1,11 +1,21 @@
+const buttonColours = ["red", "blue", "green", "yellow"];
+
 const gamePattern = [];
 
-const buttonColours = ["red", "blue", "green", "yellow"];
-const randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
-
 function nextSequence() {
-    const randomNumber = Math.floor(Math.random() * 3);
-    return randomNumber;
+    //Select a random Coloured Button
+    const randomNumber = Math.floor(Math.random() * 4);
+    const randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    //Animation
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+    //Audio
+    const audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
+
+nextSequence();
+
 
